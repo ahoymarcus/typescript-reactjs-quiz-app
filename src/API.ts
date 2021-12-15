6b98de3dd724ca8732ae6d5edd80e5ba1084b23b
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-15 11:33:33 -0300

    Definir logica basica em API.ts

---
+++ src/API.ts
@@ -0,0 +1,18 @@
+
+export enum Difficulty {
+	EASY = 'easy',
+	MEDIUM = 'medium',
+	HARD = 'hard',
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): => {
+	const endpoint = `https://opendb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+	const data = await (await fetch(endpoint)).json();
+	
+	console.log(data);
+};
+
+
+
+
+
